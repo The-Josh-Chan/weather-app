2534cc9fdbc9c2057c4a7151f6fcbab192189445
Josh <joshua.chan28@yahoo.com>
2024-02-07 11:10:55 -0500

    updating delete function to delete correct data, adding function to search for rows with certain column data

--- db_handler/db_handler.py
+++ db_handler/db_handler.py
@@ -23,10 +23,15 @@ def search_city(city):
     connection = sqlite3.connect("weather_data.db")
     cursor = connection.cursor()
     try:
+        cursor.execute(f"PRAGMA table_info({city})")
+        columns_info = cursor.fetchall()
+        column_names = []
+        for column in columns_info:
+            column_names.append(column[1])
         cursor.execute(f"select * from {city}")
         tuple_list = cursor.fetchall()
         connection.close()
-        return tuple_list
+        return tuple_list, column_names
     except sqlite3.Error as err: 
         print(err, ": Use create_city_table to pull weather data into database and try again")
         connection.close()
@@ -57,3 +62,32 @@ def update_city(user_location, weather_df):
         insert_into_table(weather_df, cursor, connection, user_location)
         print(f"{user_location} table has been created and updated, use search_city function to view updated weather data table")
         connection.close()
+
+def delete_invalid_data(user_location, column, data):
+    connection = sqlite3.connect("weather_data.db")
+    cursor = connection.cursor()
+    if check_city(user_location, "weather_data.db") == True:
+        try:
+            cursor.execute(f"DELETE from {user_location} WHERE {column} = {data}")
+            connection.commit()
+            print("Data deleted successfully.")
+        except sqlite3.Error as e:
+            print("Error deleting data:", e)
+        finally:
+            cursor.close()
+
+def get_data(user_location, column, data):
+    connection = sqlite3.connect("weather_data.db")
+    cursor = connection.cursor()
+    if check_city(user_location, "weather_data.db") == True:
+        try:
+            cursor.execute(f"SELECT date, temperature_2m FROM {user_location} WHERE {column} = {data}")
+            tuple_list = cursor.fetchall()
+            connection.close()
+            return tuple_list
+        except sqlite3.Error as e:
+            print("Error selecting data:", e)
+            return False
+
+            
+
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from api_handling import weather_api
-from data_processor import data_processor
 from db_handler import db_handler
 import cmd
 
@@ -8,7 +7,7 @@ class WeatherStation(cmd.Cmd):
     def do_create_city_table(self, line):
         user_location = input(" location: ")
         weather_data = weather_api.fetch_weather_data(user_location)
-        weather_df = data_processor.process_data(weather_data)
+        weather_df = pd.DataFrame(weather_data)
         # weather_df is a pandas dataframe with timestamps as keys and parameters as columns
         # Upload data to database
         db_handler.create_location_db(user_location, weather_df)
@@ -16,13 +15,12 @@ class WeatherStation(cmd.Cmd):
 
     def do_search_city(self, line):
         user_location = input(" City: ")
-        weather_data_tuple = db_handler.search_city(user_location)
+        weather_data_tuple, weather_data_columns = db_handler.search_city(user_location)
         # Take Weather data from sqlite3 database and process to look readable
         if weather_data_tuple == False:
             return False
         else:
-            weather_df = data_processor.process_data(weather_data_tuple)
-            weather_df.rename(columns={'0':"Date", '1':"Average Temperature [C]"})
+            weather_df = pd.DataFrame(weather_data_tuple, columns = weather_data_columns)
             print(weather_df)
             return False
 
@@ -31,8 +29,44 @@ class WeatherStation(cmd.Cmd):
         start_date = input(" Start Date (YYYYMMDD): ")
         end_date = input(" End Date (YYYYMMDD): ")
         weather_data = weather_api.fetch_weather_data_date_range(user_location, start_date, end_date)
-        weather_df = data_processor.process_data(weather_data)
+        weather_df = pd.DataFrame(weather_data)
         db_handler.update_city(user_location, weather_df)
+
+    def do_delete_data(self, line):
+        print("Which city do you want to delete data from? ")
+        user_location = input()
+        weather_data_tuple, weather_data_columns = db_handler.search_city(user_location)
+        weather_df = pd.DataFrame(weather_data_tuple, columns = weather_data_columns)
+        print(weather_df.head())
+        print("What column of data do you want to reference to delete data? ")
+        column = input()
+        print("What does the data look like in the column? ")
+        data = input()
+        print(f"Are you sure you want to delete data rows that contain {data} in the {column} column? Y/N")
+        confirm = input()
+        if confirm.lower() == "y" or confirm.lower() == "yes":
+            db_handler.delete_invalid_data(user_location, column, data)
+        else:
+            print("You did not confirm with YES, data will not be deleted, cheerios! ")
+            return False
+        
+    def do_select_range(self, line):
+        print("Which city do you want to select data from?")
+        user_location = input()
+        weather_data_tuple, weather_data_columns = db_handler.search_city(user_location)
+        weather_df = pd.DataFrame(weather_data_tuple, columns = weather_data_columns)
+        print(weather_df.head())
+        print("What column of data do you want to use as reference? ")
+        column = input()
+        print("What does the data look like in the column? ")
+        data = input()
+        tuple_list = db_handler.get_data(user_location, column, data)
+        if tuple_list == False:
+            print("Error selecting data range")
+            return False
+        else:
+            weather_df = pd.DataFrame(tuple_list)
+            print(weather_df)
 
 if __name__ == "__main__":
     WeatherStation().cmdloop()
